85049bcb553dc5985711ab968f03d381930fb9ab
Rohit Kumar Mishra <rohitkumarmishra18191999@gmail.com>
2025-12-30 21:08:42 +0530

    Fix MusicPlayer.css import path and add animated gallery background

--- src/assets/components/MusicPlayer.jsx
+++ src/assets/components/MusicPlayer.jsx
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import albumData from "../data/album";
 import "./MusicPlayer.css";
 
+
 export default function MusicPlayer({ onBack }) {
   const audioRefs = useRef([]);
   const [currentPlaying, setCurrentPlaying] = useState(null);
